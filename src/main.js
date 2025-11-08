b6e398d94784e965765925d27f71be46e1a48f6f
Ifesinachi Samrose Eke <ekesamrose@gmail.com>
2025-11-08 21:58:52 +0100

    feat: update index.html structure and add drum sound buttons

--- src/main.js
+++ src/main.js
@@ -1,1 +1,35 @@
-import './style.css'
+import "./style.css";
+const keys = document.querySelectorAll(".key");
+const btn = document.querySelectorAll("button");
+
+function playSound(e) {
+  let keyCode;
+
+  if (e.type === "click") {
+    keyCode = e.currentTarget.getAttribute("data-key");
+  } else {
+    keyCode = e.keyCode;
+  }
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+});
+
+window.addEventListener("keydown", playSound);
+btn.forEach((button) => {
+  button.addEventListener("click", playSound);
+  button.addEventListener("touchstart", playSound);
+});
